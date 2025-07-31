435c899e42c2de4e34e2e2ee3348d29e98e8038e
kevin <sistemas@fundesoemco.com>
2025-07-31 17:08:42 -0500

    Prueba de CORS robusto sin routes

--- backend/index.js
+++ backend/index.js
@@ -28,17 +28,17 @@ app.use(express.json());
 // =======================================================
 // ✅ BLOQUE DE RUTAS ACTIVAS
 // =======================================================
-const ticketRoutes = require('./routes/ticketRoutes');
-const areaRoutes = require('./routes/areaRoutes');
-const authRoutes = require('./routes/authRoutes');
-const userAreaRoutes = require('./routes/userAreaRoutes');
-const userRoutes = require('./routes/userRoutes');
+//const ticketRoutes = require('./routes/ticketRoutes');
+//const areaRoutes = require('./routes/areaRoutes');
+//const authRoutes = require('./routes/authRoutes');
+//const userAreaRoutes = require('./routes/userAreaRoutes');
+//const userRoutes = require('./routes/userRoutes');
 
-app.use('/api/areas', areaRoutes);
-app.use('/api/tickets', ticketRoutes);
-app.use('/api/user-areas', userAreaRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api', authRoutes);
+//app.use('/api/areas', areaRoutes);
+//app.use('/api/tickets', ticketRoutes);
+//app.use('/api/user-areas', userAreaRoutes);
+//app.use('/api/users', userRoutes);
+//app.use('/api', authRoutes);
 
 // =======================================================
 
